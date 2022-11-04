a1983ef8877059508612e48ad686545cd0bb574e
Rich Burdon <rich.burdon@gmail.com>
2022-11-04 15:15:50 -0400

    WS-2022.2.3 <burdon@burdon-studio-2063.local Create baseRefactoring.xml, Header.ts, ide.general.xml, project.default.xml, rainbow_brackets.xml, advancedSettings.xml, diff.xml, macOS copy.xml, terminal.xml, Default.xml, Custom.xml, DXOS.xml, find.xml, ui.lnf.xml, DontShowAgainFeedbackService.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, code.style.schemes.xml, editor-font.xml, JavaScript Testing.xml, colors.scheme.xml, notifications.xml, _@user_Darcula.icls, github.xml, templates.xml, abbrevs.xml, wallaby.xml, laf.xml, customization.xml, Default.xml, nodejs.xml, JavaScript.xml, parameter.hints.xml, intentionSettings.xml, macros.xml, vcs.xml, TypeScript Test༏ext༏.test.ts, github-copilot.xml, keymap.xml, editor.xml, uml.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/TypeScript Test༏ext༏.test.ts
@@ -0,0 +1,11 @@
+//
+// Copyright ${YEAR} DXOS.org
+//
+
+import { expect } from 'chai';
+
+describe('Test', function () {
+  it('checks sanity', async function () {
+    expect(true).to.be.true;
+  });
+});
---
+++ fileTemplates/includes/Header.ts
@@ -0,0 +1,3 @@
+//
+// Copyright ${YEAR} DXOS.org
+//
